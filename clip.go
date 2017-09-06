e37500ae66c5b7e8eac894912824b489fcc5fb3e
as <as.utf8@gmail.com>
2017-09-06 05:19:41 -0700

    add stub for linux and plan9

--- clip.go
+++ clip.go
@@ -1,2 +1,23 @@
-package clip
+// +build !windows
 // see clip_windows.go
+
+package clip
+
+type Clip struct{
+	n int
+	err error
+}
+func New() (*Clip, error){
+	return &Clip{}, nil
+}
+func (c *Clip) Read(p []byte) (n int, err error){
+	return c.n, c.err
+}
+
+func (c *Clip) Write(p []byte) (n int, err error){
+	return c.n, c.err
+}
+
+func (c *Clip) Close() (err error){
+	return c.err
+}
