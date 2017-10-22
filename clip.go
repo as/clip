d99e96e933229d1b8c399efcf41c9a66777a8da8
as <as.utf8@gmail.com>
2017-10-22 16:02:54 -0700

    add darwin

--- clip.go
+++ clip.go
@@ -1,23 +1,24 @@
 // +build !windows
-// see clip_windows.go
+// +build !darwin
 
 package clip
 
-type Clip struct{
-	n int
+type Clip struct {
+	n   int
 	err error
 }
-func New() (*Clip, error){
+
+func New() (*Clip, error) {
 	return &Clip{}, nil
 }
-func (c *Clip) Read(p []byte) (n int, err error){
+func (c *Clip) Read(p []byte) (n int, err error) {
 	return c.n, c.err
 }
 
-func (c *Clip) Write(p []byte) (n int, err error){
+func (c *Clip) Write(p []byte) (n int, err error) {
 	return c.n, c.err
 }
 
-func (c *Clip) Close() (err error){
+func (c *Clip) Close() (err error) {
 	return c.err
 }
